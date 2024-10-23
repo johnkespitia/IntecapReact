a43b9e35b346cfe0ea78058fcef74629af5305b7
alexospina7 <alexospina7@hotmail.com>
2024-10-22 21:00:11 -0500

    formulario con imagen react

---
+++ Componentes/Formulario/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const FormComponent = () => {
+  
+    return <form action='' method='post'>  
+    <label for="fname">First name:</label>
+    <br/>
+    <input type="text" id="fname" name="fname"/> 
+    <br/>
+    <label for="lname"> Last name:</label>
+    <br/>
+    <input type="text" id="lname" name="lname"/>
+    </form>
+}
+export default FormComponent
--- index.jsx
+++ index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from './Componentes/Menu'
 import HeroComponent from './Componentes/Hero'
+import FormComponent from './Componentes/Formulario'
 function App(){
     return <div>
         <Menu />
         <HeroComponent />
+        <FormComponent/>
     </div>
 }
 export default App
