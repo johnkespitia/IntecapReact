4913aa9c632fe1349d14089d36f4ab677166ba82
carlos Guampe <ceo@etivi.com>
2024-10-22 22:16:57 -0500

    Componente Web

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,36 +3,48 @@ import React from "react";
 export const Form = () => {
 	return (
 		<>
-			<form id="contact-form" class="contact-form">
-				<h2>Contáctanos</h2>
-				<div className="form-group">
-					<label for="name">Nombre:</label>
-					<input
-						type="text"
-						id="name"
-						name="name"
-						placeholder="Ingrese su nombre"
+			<div className="container-form">
+				<div>
+					<h3>Dejanos tus datos</h3>
+					<p>Ingresa tus datos y nos pondremos en contacto contigo.</p>
+					<p>¡Gracias por tu tiempo!</p>
+					<img
+						src="https://react-landing-page-template-93ne.vercel.app/img/portfolio/06-large.jpg"
+						alt="titulo"
+                        width="500px"
 					/>
 				</div>
-				<div className="form-group">
-					<label for="email">Correo electrónico:</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						placeholder="Ingrese su correo electrónico"
-					/>
-				</div>
-				<div className="form-group">
-					<label for="message">Mensaje:</label>
-					<textarea
-						id="message"
-						name="message"
-						placeholder="Ingrese su mensaje"
-					></textarea>
-				</div>
-				<button type="submit">Enviar</button>
-			</form>
+				<form id="contact-form" className="contact-form">
+					<h2>Contáctanos</h2>
+					<div className="form-group">
+						<label htmlFor="name">Nombre:</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							placeholder="Ingrese su nombre"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="email">Correo electrónico:</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Ingrese su correo electrónico"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="message">Mensaje:</label>
+						<textarea
+							id="message"
+							name="message"
+							placeholder="Ingrese su mensaje"
+						></textarea>
+					</div>
+					<button type="submit">Enviar</button>
+				</form>
+			</div>
 		</>
 	);
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,26 +7,21 @@ export const Slider = () => {
 	return (
 		<>
 			<main>
-				<ul className="slider" style={{listStyle: 'none'}}>
-					<li
+				<div className="slider">
+					<div
 						className="item"
-						style={{
-							backgroundImage:
-								"url(https://cdn.mos.cms.futurecdn.net/dP3N4qnEZ4tCTCLq59iysd.jpg)",
-						}}
 					>
 						<div className="content">
 							<h2 className="title">"Lossless Youths"</h2>
 							<p className="description">
-								{" "}
 								Lorem ipsum, dolor sit amet consectetur adipisicing
 								elit. Tempore fuga voluptatum, iure corporis inventore
 								praesentium nisi. Id laboriosam ipsam enim.{" "}
 							</p>
 							<button>Read More</button>
 						</div>
-					</li>
-				</ul>
+					</div>
+				</div>
 			</main>
 		</>
 	);
