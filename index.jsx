1f2823236efb24b0bd54314c6e84a2775d839ea2
johnkespitia <jcespitia1@gmail.com>
2024-10-22 20:56:47 -0500

    añadido formulario

--- index.jsx
+++ index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from './Componentes/Menu'
 import HeroComponent from './Componentes/Hero'
+import ContactoFormulario from './Componentes/Contacto'
 function App(){
     return <div>
         <Menu />
         <HeroComponent />
+        <ContactoFormulario />
     </div>
 }
 export default App
