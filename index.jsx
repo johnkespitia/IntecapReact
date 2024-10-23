f40e515aa4f6fc182ee847dfd9a98d0e2ce309b2
reactdevelopp <reactdevelopp@gmail.com>
2024-10-22 20:57:35 -0500

    add component Menuimg

--- index.jsx
+++ index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from './Componentes/Menu'
 import HeroComponent from './Componentes/Hero'
+import MenuImg from './Componentes/Menu/create'
 function App(){
     return <div>
         <Menu />
         <HeroComponent />
+        <MenuImg />
     </div>
 }
 export default App
