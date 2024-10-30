bc88fc431beb94917bebbbfc6e338f074913232d
carlos Guampe <ceo@etivi.com>
2024-10-29 22:08:59 -0500

    Arreglo de cartas reset - columnas de marcadores

--- Componentes/Hero/index.jsx
+++ Componentes/Hero/index.jsx
@@ -1,12 +1,69 @@
-import React, { useContext } from 'react'
-import JuegoContexto from '../../JuegoContexto'
+import React, { useContext } from "react";
+import JuegoContexto from "../../JuegoContexto";
 const HeroComponent = () => {
-    const { marcadores } = useContext(JuegoContexto)
-    return <div className="full-ancho">
-        <h2>Marcadores</h2>
-        <ul>
-            {marcadores.map((m, idx)=> <li key={idx}>{m.nickname}: {m.turnos} Turnos [Dificultad: {m.dificultad} cartas] </li>)}
-        </ul>
-    </div>
-}
-export default HeroComponent
+	const { marcadores } = useContext(JuegoContexto);
+	return (
+		<div className="full-ancho">
+			<h2>Marcadores</h2>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(3, 1fr)",
+					gap: "10px",
+				}}
+			>
+				<div>
+					<h3 style={{ color: "red" }}>Fácil</h3>
+					<ul>
+						{marcadores
+							.filter((m) => m.dificultad === 8)
+							.map((m, idx) => (
+								<div key={idx}>
+									<div>
+										<h3>{m.nickname.toUpperCase()}</h3>
+										<p>
+											Turnos : <b>{m.turnos}</b>
+										</p>
+										<p>Cartas : <b>{m.dificultad}</b></p>
+									</div>
+								</div>
+							))}
+					</ul>
+				</div>
+				<div>
+					<h3 style={{ color: "green" }}>Medio</h3>
+					<ul>
+						{marcadores
+							.filter((m) => m.dificultad === 16)
+							.map((m, idx) => (
+								<div>
+									<h3>{m.nickname.toUpperCase()}</h3>
+									<p>
+										Turnos : <b>{m.turnos}</b>
+									</p>
+									<p>Cartas : <b>{m.dificultad}</b></p>
+								</div>
+							))}
+					</ul>
+				</div>
+				<div>
+					<h3 style={{ color: "blue" }}>Difícil</h3>
+					<ul>
+						{marcadores
+							.filter((m) => m.dificultad === 32)
+							.map((m, idx) => (
+								<div>
+									<h3>{m.nickname.toUpperCase()}</h3>
+									<p>
+										Turnos : <b>{m.turnos}</b>
+									</p>
+									<p>Cartas : <b>{m.dificultad}</b></p>
+								</div>
+							))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default HeroComponent;
