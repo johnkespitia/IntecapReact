e35823424fc5bb0098386ede46f4d07d9d5e4c4d
Johnkespitia <jcespitia1@gmail.com>
2024-11-13 20:58:53 -0500

    usecallback

--- Componentes/Match/index.jsx
+++ Componentes/Match/index.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { Button } from "react-bootstrap";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 const MatchComponent = (props) => {
-    return <h1>Encuentra tu media naranja </h1>
+    const [persona, setPersona] = useState(null)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const navigate = useNavigate()
+    const genero = searchParams.get('genero')
+    const obtenerPersona = useCallback(async() => {
+        let url = 'https://randomuser.me/api'
+        if(genero){
+            url ='https://randomuser.me/api?gender='+genero 
+        }
+        const response = await fetch(url)
+        console.log(response)
+        const data =  await response.json()
+        console.log(data)
+        setPersona(data.results[0])
+    },[genero])
+
+    useEffect(()=>{
+        obtenerPersona()
+        
+    },[obtenerPersona])
+
+    return <>
+    <h1>Encuentra tu media naranja </h1>
+    <Button onClick={()=>{ navigate('/match?genero=male') }}>Hombre</Button>
+    <Button onClick={()=>{ navigate('/match?genero=female') }}>Mujer</Button>
+    <Button onClick={()=>{ obtenerPersona()} }>cambiar Persona</Button>
+    </>
 }
 export default MatchComponent
